2a20640837f534e6c7f0ee907bcc7e7acf0bc7ad
LeoPeixoto1 <128064535+LeoPeixoto1@users.noreply.github.com>
2024-07-08 08:27:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 import json
+from typing import Optional
 
 app = FastAPI()
+
 
 def ler_json(filename):
     try:
@@ -109,6 +111,9 @@ def remover_carrinho(id_carrinho: str):
         }
         
 @app.get("/products")
-def listar_todos_produtos():
+def listar_todos_produtos(categoria: Optional[str] = Query(None, alias="CATEGORIA")):
     produtos = ler_json('produtos.json')
+    if categoria:
+        produtos = [produto for produto in produtos if produto.get('categoria') == categoria]
     return produtos
+
